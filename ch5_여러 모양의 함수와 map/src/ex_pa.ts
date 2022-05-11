9346a60441267908775abf678c21ef8f26379573
kwhong95 <kwhong95@kakao.com>
2022-05-12 01:23:39 +0900

    5/12 TIL

---
+++ ch5_여러 모양의 함수와 map/src/ex_pa.ts
@@ -0,0 +1,25 @@
+const delivery = (present: string, from: string, to: string) => {
+  return `
+    보내는 물건: ${present}
+    보내는 사람: ${from}
+    받는 사람: ${to}
+  `;
+};
+
+// Patial Application
+const paDelivery = (present: string, from: string) => (to: string) => {
+  return `
+    보내는 물건: ${present}
+    보내는 사람: ${from}
+    받는 사람: ${to}
+  `;
+};
+
+export const main = () => {
+  console.clear();
+
+  const momsPresent = paDelivery("상품권", "엄마");
+  console.log(momsPresent("아들"));
+  console.log(momsPresent("딸"));
+  console.log(momsPresent("할머니"));
+};
