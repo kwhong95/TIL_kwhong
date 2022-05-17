f236421e27478f30d8de888179c25bff8ce0081d
kwhong95 <kwhong95@kakao.com>
2022-05-17 18:22:09 +0900

    [Creact-Redux-Middleware]Logger middleware

--- create-redux-middleware/app.js
+++ create-redux-middleware/app.js
@@ -2,32 +2,9 @@ import { createStore } from "./redux.js";
 import * as Actions from "./actions.js";
 import reducer from "./reducer.js";
 import { ASYNC_INCREASE_COUNTER, SET_COUNTER } from "./action-type.js";
+import { logger } from "./logger.js";
 
-const middleware1 = (store) => (next) => (action) => {
-  console.log(`m1 => `, action);
-  next(action);
-};
-
-const middleware2 = (store) => (next) => (action) => {
-  console.log(`m2 => `, action);
-  if (action.type === SET_COUNTER) {
-    action.payload = 100;
-  }
-  next(action);
-};
-
-const middleware3 = (store) => (next) => (action) => {
-  console.log(`m3 => `, action);
-  if (action.type === ASYNC_INCREASE_COUNTER) {
-    setTimeout(() => {
-      next(Actions.increase());
-    }, 1000);
-  } else {
-    next(action);
-  }
-};
-
-const store = createStore(reducer, [middleware1, middleware2, middleware3]);
+const store = createStore(reducer, [logger]);
 
 const counterDisplay = document.querySelector("#counter");
 const btnIncrease = document.querySelector("#btn-increase");
