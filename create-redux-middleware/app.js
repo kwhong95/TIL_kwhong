c49f2fa544f8fa9c3a5aa3dc16b7fecb9b69315e
kwhong95 <kwhong95@kakao.com>
2022-05-17 18:41:55 +0900

    [Creact-Redux-Middleware]Async processing middleware

--- create-redux-middleware/app.js
+++ create-redux-middleware/app.js
@@ -1,20 +1,42 @@
 import { createStore } from "./redux.js";
 import * as Actions from "./actions.js";
 import reducer from "./reducer.js";
-import { ASYNC_INCREASE_COUNTER, SET_COUNTER } from "./action-type.js";
 import { logger } from "./logger.js";
+import { ASYNC_INCREASE_COUNTER } from "./action-type.js";
 
-const store = createStore(reducer, [logger]);
+const asyncRouter = (jobs) => (store) => (next) => (action) => {
+  const matchJob = Object.entries(jobs).find(([type]) => action.type === type);
+
+  if (matchJob) {
+    matchJob[1](store, action);
+  } else {
+    next(action);
+  }
+};
+
+const asyncJobs = {
+  [ASYNC_INCREASE_COUNTER]: function (store, action) {
+    store.dispatch(Actions.asyncRequest());
+    setTimeout(() => {
+      store.dispatch(Actions.increase(20));
+      store.dispatch(Actions.asyncResponse());
+    }, 3000);
+  },
+};
+
+const store = createStore(reducer, [logger, asyncRouter(asyncJobs)]);
 
 const counterDisplay = document.querySelector("#counter");
+const loadingMessage = document.querySelector("#loading");
 const btnIncrease = document.querySelector("#btn-increase");
 const btnAsyncIncrease = document.querySelector("#btn-async-increase");
 const btnDecrease = document.querySelector("#btn-decrease");
 const btnReset = document.querySelector("#btn-reset");
 
 store.subscribe(function () {
-  const { counter } = store.getState();
+  const { counter, request } = store.getState();
 
+  loadingMessage.style.visibility = request ? "visible" : "hidden";
   counterDisplay.textContent = counter;
 });
 
